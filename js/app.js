69765d075f2884894914ea39e85ee50b90f3575a
AndresCagua <andrescagua-88@hotmail.com>
2022-10-11 17:16:57 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+console.log('Aplicación Calculadora');
+const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    
+
+function sumar(){
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function restar(){
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function multiplicar(){
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function dividir(){
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
